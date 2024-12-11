43a72ddb77ac4f65bf938f94a2c8a690424bcded
Abigailhdz04 <abigailhe.ti23@utsjr.edu.mx>
2024-12-11 10:45:13 -0600

    agregar todos los archivos

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+export default function Index() {
+    return (
+        <div>
+            <h1>Inicio</h1>
+            <p>Hola mundo</p>
+            
+            <p>Hola profe, asi como nos hizo caso la vez que le dijimos que nos pusiera 10 y nos puso 10 de 100 , asi pasenos (apruebenos) ahora por favor profe :), Bonito dia y felices fiestas si si nos aprueba y ya no lo vemos en acc.</p> 
+        </div>
+    );
+}
